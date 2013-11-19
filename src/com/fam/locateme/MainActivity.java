e912deeb3074f7b073000cebae38e36cc3009109
flotre <flo@flo.flo>
2013-11-19 23:42:05 +0100

    sms received and display in textview

---
+++ src/com/fam/locateme/MainActivity.java
@@ -0,0 +1,64 @@
+package com.fam.locateme;
+
+import android.app.*;
+import android.os.*;
+import android.view.*;
+import android.widget.*;
+import android.telephony.SmsManager;
+
+import android.content.BroadcastReceiver;
+import android.content.IntentFilter;
+import android.content.Context;
+import android.content.Intent;
+
+
+public class MainActivity extends Activity
+{
+	IntentFilter intentFilter;
+	
+	
+	private BroadcastReceiver intentReceiver = new BroadcastReceiver()
+	{
+		@Override
+		public void onReceive(Context context, Intent intent)
+		{
+			// display sms received in textview
+			TextView SMSes = (TextView) findViewById(R.id.console);
+			SMSes.setText(intent.getExtras().getString("sms"));
+		}
+	};
+	
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+	{
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+		
+		// intent to filter for sms messages received
+		intentFilter = new IntentFilter();
+		intentFilter.addAction("SMS_RECEIVED_ACTION");
+    }
+	
+	private void SendSms(String phone_number, String message)
+	{
+		SmsManager manager = SmsManager.getDefault();
+		manager.sendTextMessage(phone_number, null, message, null, null);
+	}
+	
+	@Override
+	protected void onResume()
+	{
+		//register the receiver
+		registerReceiver(intentReceiver, intentFilter);
+		super.onResume();
+	}
+	
+	@Override
+	protected void onPause()
+	{
+		// unregister the receiver
+		unregisterReceiver(intentReceiver);
+		super.onPause();
+	}
+}
---
+++ src/com/fam/locateme/SmsReceiver.java
@@ -0,0 +1,45 @@
+package com.fam.locateme;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.telephony.SmsMessage;
+import android.widget.Toast;
+
+public class SmsReceiver extends BroadcastReceiver
+{
+
+	@Override
+	public void onReceive(Context context, Intent intent)
+	{
+		// get the sms message passed in
+		Bundle bundle = intent.getExtras();
+		SmsMessage[] msgs = null;
+		String str = "";
+		if( bundle != null)
+		{
+			// retrieve sms message received
+			Object[] pdus = (Object[]) bundle.get("pdus");
+			msgs = new SmsMessage[pdus.length];
+			for( int i=0; i<msgs.length; i++)
+			{
+				msgs[i] = SmsMessage.createFromPdu((byte[])pdus[i]);
+				str += "sms from " + msgs[i].getOriginatingAddress();
+				str += " :";
+				str += msgs[i].getMessageBody().toString();
+				str += "\n";
+			}
+			// display message
+			Toast.makeText(context,str,Toast.LENGTH_SHORT).show();
+			
+			// send to other activity
+			Intent broadcastIntent = new Intent();
+			broadcastIntent.setAction("SMS_RECEIVED_ACTION");
+			broadcastIntent.putExtra("sms",str);
+			context.sendBroadcast(broadcastIntent);
+		}
+	}
+
+
+}
