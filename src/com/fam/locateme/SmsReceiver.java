9a8b27c8fb9031374ed20bc3aa9f04b01a7198cf
flotre <flo@flo.flo>
2013-11-20 20:16:23 +0100

    add location sms response

--- src/com/fam/locateme/SmsReceiver.java
+++ src/com/fam/locateme/SmsReceiver.java
@@ -5,10 +5,17 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.telephony.SmsMessage;
+import android.telephony.SmsManager;
 import android.widget.Toast;
+import android.location.*;
+
+
 
 public class SmsReceiver extends BroadcastReceiver
 {
+	String receiver_tel_number;
+	LocationManager loc_manager;
+	LocationListener  loc_listener;
 
 	@Override
 	public void onReceive(Context context, Intent intent)
@@ -17,6 +24,7 @@ public class SmsReceiver extends BroadcastReceiver
 		Bundle bundle = intent.getExtras();
 		SmsMessage[] msgs = null;
 		String str = "";
+		
 		if( bundle != null)
 		{
 			// retrieve sms message received
@@ -25,11 +33,32 @@ public class SmsReceiver extends BroadcastReceiver
 			for( int i=0; i<msgs.length; i++)
 			{
 				msgs[i] = SmsMessage.createFromPdu((byte[])pdus[i]);
-				str += "sms from " + msgs[i].getOriginatingAddress();
-				str += " :";
+				if( i== 0)
+				{
+					receiver_tel_number = msgs[i].getOriginatingAddress(); 
+				}
+				
 				str += msgs[i].getMessageBody().toString();
-				str += "\n";
 			}
+			
+			// test message content
+			if( str.startsWith("#WRU#") )
+			{
+				// location request
+				loc_manager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+				loc_listener = new MyLocationListener();			
+				loc_manager.requestLocationUpdates(
+						LocationManager.NETWORK_PROVIDER,
+						60000,
+						1000,
+						loc_listener);
+			
+				// message only for this application
+				this.abortBroadcast();
+			}
+			
+			
+			
 			// display message
 			Toast.makeText(context,str,Toast.LENGTH_SHORT).show();
 			
@@ -43,5 +72,35 @@ public class SmsReceiver extends BroadcastReceiver
 		}
 	}
 
-
+	
+	
+	private class MyLocationListener implements LocationListener
+	{
+		
+		public void onLocationChanged(Location loc)
+		{
+			if(loc != null)
+			{
+				String message = "http://maps.google.com/maps?q="+
+						loc.getLatitude()+", "+loc.getLongitude();
+				
+				SmsManager manager = SmsManager.getDefault();
+				manager.sendTextMessage(receiver_tel_number, null, message, null, null);
+			
+				loc_manager.removeUpdates(loc_listener);
+			}
+		}	
+		
+		
+		public void onProviderDisabled(String provider)
+		{
+		}
+		public void onProviderEnabled(String provider)
+		{
+		}
+		public void onStatusChanged(String provider,int status,Bundle extra)
+		{
+		}
+	}
+	
 }
